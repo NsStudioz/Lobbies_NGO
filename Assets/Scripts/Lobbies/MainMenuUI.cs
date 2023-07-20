6a7de90051bb932f13b67c9a0b2d17ff59d210b1
NsStudioz <sneve2016@gmail.com>
2023-07-20 10:47:36 +0300

    Update: MainMenuUI = logic to show lobby panels.

--- Assets/Scripts/Lobbies/MainMenuUI.cs
+++ Assets/Scripts/Lobbies/MainMenuUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,24 +7,55 @@ using UnityEngine.UI;
 public class MainMenuUI : MonoBehaviour
 {
 
-    [SerializeField] private GameObject createLobbyMenu;
-    [SerializeField] private GameObject joinLobbyMenu;
+    [SerializeField] private GameObject createLobbyPanel;
+    [SerializeField] private GameObject joinLobbyPanel;
     [SerializeField] private GameObject menuPanel;
 
     [SerializeField] private Button createLobbyButton;
     [SerializeField] private Button joinLobbyButton;
     [SerializeField] private Button optionsButton;
 
+    // TO-DO:
+    // Options button, options panel & Show Options menu
+
+    private void Start()
+    {
+        ShowMainMenuPanel();
+    }
+
     private void OnEnable()
     {
-        
+        createLobbyButton.onClick.AddListener(ShowCreateLobbyPanel);
+        joinLobbyButton.onClick.AddListener(ShowJoinLobbyPanel);
     }
 
     private void OnDisable()
     {
-
+        createLobbyButton.onClick.RemoveAllListeners();
+        joinLobbyButton.onClick.RemoveAllListeners();
+        optionsButton.onClick.RemoveAllListeners();
     }
 
+    private void ShowMainMenuPanel()
+    {
+        menuPanel.SetActive(true);
+        createLobbyPanel.SetActive(false);
+        joinLobbyPanel.SetActive(false);
+    }
+
+    private void ShowCreateLobbyPanel()
+    {
+        createLobbyPanel.SetActive(true);
+        joinLobbyPanel.SetActive(false);
+        menuPanel.SetActive(false);
+    }
+
+    private void ShowJoinLobbyPanel()
+    {
+        joinLobbyPanel.SetActive(true);
+        createLobbyPanel.SetActive(false);
+        menuPanel.SetActive(false);
+    }
 
 
 }
