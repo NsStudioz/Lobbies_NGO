e6b8acf1e78a7fba16e900ff2ed563c5529c1729
NsStudioz <sneve2016@gmail.com>
2023-07-20 10:34:11 +0300

    New: MainMenuUI = some vars.

---
+++ Assets/Scripts/Lobbies/MainMenuUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+
+    [SerializeField] private GameObject createLobbyMenu;
+    [SerializeField] private GameObject joinLobbyMenu;
+    [SerializeField] private GameObject menuPanel;
+
+    [SerializeField] private Button createLobbyButton;
+    [SerializeField] private Button joinLobbyButton;
+    [SerializeField] private Button optionsButton;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+
+
+}
