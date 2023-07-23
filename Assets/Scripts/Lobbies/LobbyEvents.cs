12b7e369d7b29a5af304c5808583294a82b4744f
NsStudioz <sneve2016@gmail.com>
2023-07-23 15:45:05 +0300

    New: LobbyEvents.cs = new event.

---
+++ Assets/Scripts/Lobbies/LobbyEvents.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyEvents
+{
+
+    public delegate void LeaveLobby();
+    public static LeaveLobby OnLeaveLobby;
+
+}
