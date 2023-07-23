a2aa42b1af9422b2b971b7bc33f4aa975abdb6c3
NsStudioz <sneve2016@gmail.com>
2023-07-23 11:39:42 +0300

    Update: LobbyManagerUI.cs = Leave Lobby button function.

--- Assets/Scripts/Lobbies/LobbyManagerUI.cs
+++ Assets/Scripts/Lobbies/LobbyManagerUI.cs
@@ -1,9 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyManagerUI : MonoBehaviour
 {
+
+    [SerializeField] private Button LeaveLobbyBtn;
+
+    private void OnEnable()
+    {
+        LeaveLobbyBtn.onClick.AddListener(LeaveLobby);
+    }
+
+    private void OnDisable()
+    {
+        LeaveLobbyBtn.onClick.RemoveAllListeners();
+    }
+
+    private void LeaveLobby()
+    {
+        // invoke event leave lobby (Host und Client)
+    }
 
 
 }
