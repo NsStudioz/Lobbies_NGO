3e0de556afc46b86fb9cc4b734eec4f91d263b9e
NsStudioz <sneve2016@gmail.com>
2023-07-23 11:58:44 +0300

    Update: LobbyManager.cs = TryCatch functions + IsLobbyHost method.

--- Assets/Scripts/Lobbies/LobbyManager.cs
+++ Assets/Scripts/Lobbies/LobbyManager.cs
@@ -1,10 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class LobbyManager : MonoBehaviour
 {
 
+    Lobby currentLobby;
 
+    public bool IsLobbyHost()
+    {
+        return currentLobby != null && currentLobby.HostId == AuthenticationService.Instance.PlayerId;
+    }
+
+
+    private async Task<bool> TryCatchAsyncBool(Task promise)
+    {
+        try
+        {
+            await promise;
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+            return false;
+        }
+
+        return true;
+    }
+
+    public async Task<bool> CurrentLobby_TryCatchAsyncBool(Task promise)
+    {
+        if (currentLobby != null)
+        {
+            try
+            {
+                await promise;
+            }
+            catch (LobbyServiceException e)
+            {
+                Debug.Log(e);
+                return false;
+            }
+
+            return true;
+        }
+        else // if it is null
+        {
+            return false;
+        }
+
+    }
 
 }
