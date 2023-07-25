3b4b3e35677f8372184c102e2130468ebba6d5d0
NsStudioz <sneve2016@gmail.com>
2023-07-25 11:41:53 +0300

    Update: LobbyManagerUI.cs = total player in lobby text update.

--- Assets/Scripts/Lobbies/LobbyManagerUI.cs
+++ Assets/Scripts/Lobbies/LobbyManagerUI.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LobbyManagerUI : MonoBehaviour
 {
-
+    [Header("Buttons")]
     [SerializeField] private Button LeaveLobbyBtn;
+
+    [Header("Texts")]
+    [SerializeField] private TMP_Text totalPlayersInLobby;
 
     private void Start()
     {
@@ -22,6 +27,11 @@ public class LobbyManagerUI : MonoBehaviour
     private void LeaveLobby()
     {
         LobbyEvents.OnLeaveLobby?.Invoke();
+    }
+
+    private void UpdateTotalPlayersInLobbyText(Lobby currentLobby)
+    {
+        totalPlayersInLobby.text = currentLobby.Players.ToString() + "/" + currentLobby.MaxPlayers.ToString();
     }
 
 
