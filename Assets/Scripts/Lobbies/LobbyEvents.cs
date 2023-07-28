c6f66ea9fc91160fee8aa98f90b4a423e5ba0671
NsStudioz <sneve2016@gmail.com>
2023-07-28 14:32:03 +0300

    Update: LobbyEvents.cs = Organized code to be more readable.

--- Assets/Scripts/Lobbies/LobbyEvents.cs
+++ Assets/Scripts/Lobbies/LobbyEvents.cs
@@ -5,26 +5,31 @@ using UnityEngine;
 
 public class LobbyEvents
 {
+
+    // Lobby_Create:
     public delegate void CreateLobby();
     public static CreateLobby OnCreateLobby;
 
     public delegate void LobbyCreated(string lobbyCode);
     public static LobbyCreated OnLobbyCreated;
 
+    // Lobby_Join:
     public delegate void JoiningLobbyByCode(string lobbyCode);
     public static JoiningLobbyByCode OnJoiningLobbyByCode;
+
+    public delegate void JoinedLobby();
+    public static JoinedLobby OnJoinedLobby;
+
+    // Lobby_Leave:
+    public delegate void LeaveLobby();
+    public static LeaveLobby OnLeaveLobby;
 
     public delegate void LeaveJoinLobbyUI();
     public static LeaveJoinLobbyUI OnLeaveJoinLobbyUI;
 
-    public delegate void LeaveLobby();
-    public static LeaveLobby OnLeaveLobby;
-
+    // Lobby_Update
     public delegate void LobbyUpdated(Lobby currentLobby);
     public static LobbyUpdated OnLobbyUpdated;
-
-    public delegate void JoinedLobby();
-    public static JoinedLobby OnJoinedLobby;
 
     public delegate void LobbyPrivacyStateChange(bool state);
     public static LobbyPrivacyStateChange OnLobbyPrivacyStateChange;
