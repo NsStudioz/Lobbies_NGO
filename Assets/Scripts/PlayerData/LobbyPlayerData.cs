7dbb52df82db995877529db1970f27d1bfd8fb1d
NsStudioz <sneve2016@gmail.com>
2023-08-01 21:05:39 +0300

    Update: LobbyPlayerData.cs = Player name system.

--- Assets/Scripts/PlayerData/LobbyPlayerData.cs
+++ Assets/Scripts/PlayerData/LobbyPlayerData.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyPlayerData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI playerName;
+    private readonly string emptyPlayerName = "<EMPTY SLOT>";
+
+    private Player player;
+
+    private void Start()
     {
-        
+        ResetPlayerNameText();
     }
 
-    // Update is called once per frame
-    void Update()
+    // For when a player in not populating this data.
+    private void ResetPlayerNameText() 
     {
-        
+        playerName.text = emptyPlayerName;
     }
+
+    public void UpdatePlayerName(Player player)
+    {
+        this.player = player;
+        playerName.text = this.player.Data[LobbyManager.KEY_PLAYER_NAME].Value;
+    }
+
 }
