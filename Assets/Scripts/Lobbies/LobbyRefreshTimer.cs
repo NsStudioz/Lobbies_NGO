4da6181232aca471c43e1c174ba23ab37fbef459
NsStudioz <sneve2016@gmail.com>
2023-08-03 15:55:04 +0300

    Update: LobbyRefreshTimer.cs = New function for lobby refresh.

--- Assets/Scripts/Lobbies/LobbyRefreshTimer.cs
+++ Assets/Scripts/Lobbies/LobbyRefreshTimer.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class LobbyRefreshTimer : MonoBehaviour
 {
     [SerializeField] private float lobbyPollTimer;
+    [SerializeField] private float lobbyPollTimerThreshold = 1.1f;
 
     private void Update()
     {
-        
+        StartTimerLoop(Time.deltaTime);
     }
 
+    private void StartTimerLoop(float deltaTime)
+    {
+        if (LobbyManager.Instance.GetCurrentLobby() == null)
+            return;
 
+        else
+        {
+            lobbyPollTimer -= deltaTime;
+            if (lobbyPollTimer <= 0)
+            {
+                lobbyPollTimer = lobbyPollTimerThreshold;
+                LobbyEvents.OnTriggerLobbyRefresh?.Invoke();
+            }
+        }
+
+    }
 
 }
