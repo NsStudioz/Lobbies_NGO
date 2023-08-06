68cf07bb7d5fc024dd1e6a74158a6f1b299dfc5e
NsStudioz <sneve2016@gmail.com>
2023-08-06 12:35:46 +0300

    Update: PlayerAvatar.cs = new getsprite function

--- Assets/Scripts/PlayerData/PlayerAvatar.cs
+++ Assets/Scripts/PlayerData/PlayerAvatar.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
-public class PlayerAvatar
+
+public class PlayerAvatar: MonoBehaviour
 {
+    public static PlayerAvatar Instance;
 
-    [SerializeField] private List<Sprite> playerAvatarList = new List<Sprite>(4);
+    private void Awake()
+    {
+        Instance = this;
+    }
 
+    [SerializeField] private List<Sprite> playerAvatarList = new List<Sprite>(5);
+    //  0 = Heart, 1 = Diamond, 2 = Gold, 3 = Star, 4 = Lightning
 
+    public Sprite GetSprite(LobbyManager.PlayerAvatarEnum playerAvatar)
+    {
+        return playerAvatar switch
+        {
+            LobbyManager.PlayerAvatarEnum.Diamond   => playerAvatarList[1],
+            LobbyManager.PlayerAvatarEnum.Gold      => playerAvatarList[2],
+            LobbyManager.PlayerAvatarEnum.Star      => playerAvatarList[3],
+            LobbyManager.PlayerAvatarEnum.Lightning => playerAvatarList[4],
+            _ => playerAvatarList[0], // default = Heart
+        };
+    }
 
 }
