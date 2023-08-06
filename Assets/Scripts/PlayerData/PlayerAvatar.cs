be515bd69014050f56679c88d679a6b157a5ce50
NsStudioz <sneve2016@gmail.com>
2023-08-06 11:06:44 +0300

    New: Player avatar script.

---
+++ Assets/Scripts/PlayerData/PlayerAvatar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class PlayerAvatar
+{
+
+    [SerializeField] private List<Sprite> playerAvatarList = new List<Sprite>(4);
+
+
+
+}
