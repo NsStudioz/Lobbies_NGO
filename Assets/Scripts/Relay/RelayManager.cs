2c4d18626e67e9b56f58890e0baca46920c61e7c
NsStudioz <sneve2016@gmail.com>
2023-08-07 13:28:46 +0300

    Update: RelayManager.cs = New var.

--- Assets/Scripts/Relay/RelayManager.cs
+++ Assets/Scripts/Relay/RelayManager.cs
@@ -6,6 +6,8 @@ public class RelayManager : MonoBehaviour
 {
 
     public static RelayManager Instance;
+    private const int maxPlayers = 3; // only clients are counted here, host is already considered as a connection,
+                                      // therefore the host is not included as a connection here.
 
     void Awake()
     {
