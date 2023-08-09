1fa192a6416faac09e11bad90ed0f64e64eb0228
NsStudioz <sneve2016@gmail.com>
2023-08-09 17:08:48 +0300

    Update: JoinLobbyUI.cs = new lists.

--- Assets/Scripts/Lobbies/JoinLobbyUI.cs
+++ Assets/Scripts/Lobbies/JoinLobbyUI.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class JoinLobbyUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private List<Lobby> lobbiesToQuery = new List<Lobby>(6);                     // Available public lobbies.
+    [SerializeField] private List<TMP_Text> lobbyNames = new List<TMP_Text>();   // Available public lobbies.
+    [SerializeField] private List<TMP_Text> lobbyPlayers = new List<TMP_Text>(); // Available players in public lobby.
+
 }
